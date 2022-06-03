389042bdae2de13d70d7183891c74bdc2344405e
ChiragBangera <74919257+ChiragBangera@users.noreply.github.com>
2022-06-03 21:05:42 +0530

    set

--- Python course/dictionaries/precription/perscription_processing.py
+++
@@ -1,9 +0,0 @@
-from prescription_data import patients
-
-trial_patients = ["Denise", "Frank", "Georgia", "Kenny", "Frank"]
-
-while trial_patients:
-    patient = trial_patients.pop()
-    print(patient, end=" : ")
-    prescription = patients[patient]
-    print(prescription)
