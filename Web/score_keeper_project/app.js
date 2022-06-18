11c802f1a49312aed99e3b9eaeace7e3789d620f
ChiragBangera <chiragbangera468@gmail.com>
2022-06-18 10:24:29 +0530

    score

--- Web/score_keeper_project/app.js
+++ Web/score_keeper_project/app.js
@@ -1,48 +1,63 @@
-const p1 = {
-    score:0,
-    button: document.querySelector('#p1b'),
-    display: document.querySelector('#p1d')
+const p1= {
+    score : 0,
+    display : document.querySelector('#p1d')
 }
-const p2 = {
-    score:0,
-    button: document.querySelector('#p2b'),
-    display: document.querySelector('#p2d')
+const p2= {
+    score : 0,
+    display : document.querySelector('#p2d')
 }
 
 
-let winningScore = 3
+
+const pclick = document.querySelectorAll('button');
+const win = document.querySelector('#game-selector')
+
+let winningScore = 5
 let won = false
 
 
 
-const win = document.querySelector('#game-selector')
-const reset = document.querySelector('#resb')
-
-p1.button.addEventListener('click',()=>{
-    console.log("sfvjb")
+pclick.forEach(click =>{
+    click.addEventListener('click',(e)=>{
+        const clicked = e.target.id;
+        console.log(clicked)
+        if (clicked==='p1b'){
+            addScore(p1,p2)
+        }
+        else if (clicked==='p2b'){
+            addScore(p2,p1)
+        }
+        else if (clicked==='resb'){
+            reset()
+        }
+        console.log(winningScore)
+        
+    })
 })
 
-
-function addScore(player) {
+function addScore(player,opponent){
     if (!won){
-        player.score+=1;
-        if (player.score===winningScore){
-            won=true
+        player.score+=1
+        if (player.score === winningScore){
+            player.display.style.color = 'Green'
+            opponent.display.style.color = 'red'
+            won=true;
         }
         player.display.textContent = player.score
     }
 }
 
+win.addEventListener('change',()=>{
+    winningScore = parseInt(win.value);
+    reset();
+})
 
-function reset() {
-    won = false
+
+function reset(){
     for (let p of [p1,p2]){
-        p2.score = 0
-        p1.display.textContent=0
-        p1.button.disabled = false
+        won = false
+        p.score = 0
+        p.display.textContent = 0
+        p.display.style.color = 'black'
     }
 }
-
-win.addEventListener('change',()=>{
-    winningScore = parseInt(this.value);
-})
