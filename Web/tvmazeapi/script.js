b31c77c8508a1d969a170bb8cc9a05253b61ba0d
ChiragBangera <chiragbangera468@gmail.com>
2022-06-24 23:05:55 +0530

    set1

--- Web/tvmazeapi/script.js
+++ Web/tvmazeapi/script.js
@@ -1,13 +1,11 @@
 const form  = document.querySelector('#searchForm')
-form.addEventListener('submit',async (e)=>{
+const image  = document.querySelector('#inpimages')
+const photo  = document.querySelector('img')
+const btn = document.querySelector('button')
+
+form.addEventListener('submit',(e)=>{
     e.preventDefault()
-    const inpValue = form.elements.query.value
-    console.log(inpValue)
-    const query = await axios.get(`https://api.tvmaze.com/search/shows?q=${inpValue}`)
-    const data = query.data
-    console.log(data) 
-    addImages(data)
-    form.elements.query.value = '';
+    dataquery()
 })
 
 const addImages = (shows)=>{
@@ -16,8 +14,24 @@ const addImages = (shows)=>{
         if (img){
             const imgcon = document.createElement('img');
             imgcon.src = img.medium
-            document.body.append(imgcon)
+            image.append(imgcon)
         }
     }
 }
+
+
+const dataquery = async ()=>{
+    const inpValue = form.elements.query.value
+    console.log(inpValue)
+    const query = await axios.get(`https://api.tvmaze.com/search/shows?q=${inpValue}`)
+    const data = query.data
+    console.log(data)
+    image.innerHTML = ''
+    addImages(data)
+    form.elements.query.value = '';
+}
+
+
+
+
 
