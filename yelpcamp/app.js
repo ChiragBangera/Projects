10dbfcf9e2e5e1b5c7917ae9fee5725b0d50ea32
ChiragBangera <chiragbangera468@gmail.com>
2022-07-13 02:29:48 +0530

    yelp

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -5,7 +5,8 @@ const Campground = require('./models/campground')
 const methodoverride = require('method-override')
 const mongoose = require('mongoose')
 const engine = require('ejs-mate')
-
+const ExpressError = require('./error/errorhandlers')
+const catchAsync = require('./error/catchasync')
 
 
 const DB = "mongodb+srv://chiragbangera:chiragadarsh@cluster1.c3lu4j0.mongodb.net/yelpcamp?retryWrites=true&w=majority"
@@ -21,6 +22,8 @@ mongoose.connect(DB,
         console.log('MongoDB Not Connected')
         console.log(e)
     })
+
+
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
@@ -40,10 +43,10 @@ app.get('/',(req,res)=>{
 })
 
 // Campground page
-app.get('/campgrounds',async (req,res)=>{
+app.get('/campgrounds',catchAsync(async (req,res)=>{
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index',{campgrounds})
-})
+}))
 
 // new Camp
 app.get('/campgrounds/newcamp',(req,res)=>{
@@ -51,40 +54,58 @@ app.get('/campgrounds/newcamp',(req,res)=>{
 })
 
 // new camp post
-app.post('/campgrounds',async (req,res)=>{
+app.post('/campgrounds',catchAsync(async (req,res)=>{
     const newCamp = new Campground(req.body)
     await newCamp.save()
     res.redirect(`/campgrounds/${newCamp._id}`)
-})
+}))
 
 // campground show route
-app.get('/campgrounds/:id',async (req,res)=>{
+app.get('/campgrounds/:id',catchAsync(async (req,res)=>{
     const {id} = req.params
     const camp = await Campground.findById(id)
+    if(!camp){
+        throw new ExpressError(401,'Camp Not Found')
+    }
     res.render('campgrounds/show',{camp})
-})
+}))
 
 // edit page route
-app.get('/campgrounds/:id/editcamp',async (req,res)=>{
+app.get('/campgrounds/:id/editcamp',catchAsync(async (req,res)=>{
     const {id} = req.params;
     const findCamp = await Campground.findById(id)
     res.render('campgrounds/editcamp',{findCamp})
-})
+}))
 
 // Updating the camp
-app.patch('/campgrounds/:id',async(req,res)=>{
+app.patch('/campgrounds/:id',catchAsync(async(req,res)=>{
     const {id} = req.params;
     const updatedCamp = req.body;
     await Campground.findByIdAndUpdate(id,updatedCamp)
     res.redirect(`/campgrounds/${id}`)
-})
+}))
 
 // deleting camp
-app.delete('/campgrounds/:id',async(req,res)=>{
+app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
     const {id} = req.params;
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')
+}))
+
+
+
+
+
+
+
+app.use((err,req,res,next)=>{
+    const {status=500,message="Something went wrong"} = err
+    res.status(status).render('error', {status,message})
 })
+
+
+
+
 
 
 
